322a3b1a33649c53b57f4b755de7578039bbe556
guisalmeida <guisalmeida.dev@gmail.com>
2024-09-03 21:36:37 -0300

    Chore: create home view

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-3xl font-bold underline">Home</h1>
+  <section class="h-screen w-full flex flex-col align items-center justify-center">
+    <header
+      className="flex flex-col align items-center justify-center max-w-6xl py-10 mx-auto gap-3"
+    >
+      <div className="flex flex-col align items-center justify-center gap-2">
+        <img src="../assets/images/logo.svg" alt="Planner logo" />
+
+        <p className="text-sm text-zinc-300">Convide seus amigos e planeje sua próxima viagem!</p>
+      </div>
+    </header>
+
+    <h1 class="text-3xl font-bold underline">Home</h1>
+  </section>
 </template>
